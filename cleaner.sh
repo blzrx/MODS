9b366398b98c2498c3d925a78bcca6d6b4d0882a
Alvaro D <blzrx@icloud.com>
2016-08-29 16:07:31 -0400

    Create cleaner.sh

---
+++ cleaner.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#MODS cleaner!
+#
+
+filename=`cat last.file`
+
+#Some common Cisco trash output lines
+
+ex +g/User/d -cwq /home/mods/results/$filename
+ex +g/Password:/d -cwq /home/mods/results/$filename
+ex +g/Trying/d -cwq /home/mods/results/$filename
+ex +g/spawn/d -cwq /home/mods/results/$filename
+ex +g/Connected/d -cwq /home/mods/results/$filename
+ex +g/Escape/d -cwq /home/mods/results/$filename
+
+#EXAMPLE
+#If you want to delete the EXEC word in the file, put it between +g/ and /d, like this
+#ex +g/EXEC/d -cwq /home/itserv/ondiag/results/$filename
+
+echo ""
+echo "File $filename cleaned!"
+echo "Exiting..."
